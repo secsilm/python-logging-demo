ac15b475d61b2047f635198b49334d10f22ec5a0
secsilm <secsilm@outlook.com>
2019-12-30 16:43:13 +0800

    Reformat

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from package_a import module_a
 with open("logging_config.yml", "r", encoding="utf8") as f:
     logging_config = yaml.safe_load(f)
 logging.config.dictConfig(logging_config)
-logger = logging.getLogger('app')
+logger = logging.getLogger("app")
 
 logger.debug("来自 app 的 DEBUG 日志。")
 logger.warning("来自 app 的 WARNING 日志。")
--- package_a/module_a.py
+++ package_a/module_a.py
@@ -2,6 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def print_log():
-    logger.debug('来自 module_a 的 DEBUG 日志')
-    logger.warning('来自 module_a 的 WARNING 日志')
+    logger.debug("来自 module_a 的 DEBUG 日志")
+    logger.warning("来自 module_a 的 WARNING 日志")
